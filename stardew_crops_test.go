c63d8c1ee7d4cb7b4cfafb4a6c909c52cd42d002
Jayson Smith <github@nes.33mail.com>
2018-12-09 09:52:58 -0700

    Add godog file

--- stardew_crops_test.go
+++ stardew_crops_test.go
@@ -6,6 +6,7 @@ import (
 )
 
 func FeatureContext(s *godog.Suite) {
+	// testSc
 	s.Step(`^the pretty formatter is desired$`, steps.ThePrettyFormatterIsDesired)
 	s.Step(`^info for garlic is requested$`, steps.InfoForGarlicIsRequested)
 	s.Step(`^the output must match$`, steps.TheOutputMustMatch)
