9ea91cc8f900350474cedc47d8f9447cec24b62d
jaysonesmith <github@nes.33mail.com>
2018-12-15 15:48:50 -0700

    Update search command

--- commands/search.go
+++ commands/search.go
@@ -15,29 +15,18 @@ var searchCmd = &cobra.Command{
 	Run:   Search,
 }
 
-// SearchFlags is a collection of the supported
-// flags for the search command
-type SearchFlags struct {
-	Bundle     string
-	Continuous bool
-	GrowthGT   int64
-	GrowthLT   int64
-	Season     string
-	Trellis    bool
-}
-
-var sf SearchFlags
-
 // Search is the main orchestrator for the cli's
 // search functionality
 func Search(cmd *cobra.Command, args []string) {
+	userFlags := userSetFlags(cmd)
+
 	// return help information if no flags are provided
-	if !userSetFlags(cmd) {
+	if len(userFlags) == 0 {
 		cmd.Help()
 		return
 	}
 
-	out, err := processors.Search(args...)
+	out, err := processors.Search(userFlags)
 	if err != nil {
 		output.Print(err.Error())
 		return
@@ -49,25 +38,25 @@ func Search(cmd *cobra.Command, args []string) {
 // userSetFlags checks for flags that were
 // explicitly set to determine if any were set by
 // the user
-func userSetFlags(cmd *cobra.Command) (found bool) {
-	found = false
+func userSetFlags(cmd *cobra.Command) map[string]interface{} {
+	userFlags := make(map[string]interface{}, 0)
 	cmd.Flags().Visit(func(f *pflag.Flag) {
 		if f.Changed {
-			found = true
+			userFlags[f.Name] = f.Value
 		}
 	})
 
-	return
+	return userFlags
 }
 
 func init() {
 	// set flags
-	searchCmd.Flags().StringVarP(&sf.Bundle, "bundle", "b", "", "Specify a bundle name to search for")
-	searchCmd.Flags().BoolVarP(&sf.Continuous, "continuous", "c", false, "Search for crops that grow continuously")
-	searchCmd.Flags().Int64VarP(&sf.GrowthGT, "growthgt", "g", 0, "Search for crops that take the specified time or longer to grow")
-	searchCmd.Flags().Int64VarP(&sf.GrowthLT, "growthlt", "l", 0, "Search for crops that take the specified time or less to grow")
-	searchCmd.Flags().StringVarP(&sf.Season, "season", "s", "", "Search by a season. Options: Spring, Summer, Fall")
-	searchCmd.Flags().BoolVarP(&sf.Trellis, "trellis", "t", false, "Search for crops. Options: Spring, Summer, Fall")
+	searchCmd.Flags().StringP("bundle", "b", "", "Specify a bundle name to search for")
+	searchCmd.Flags().BoolP("continuous", "c", false, "Search for crops that grow continuously")
+	searchCmd.Flags().Int64P("growthgt", "g", 0, "Search for crops that take the specified time or longer to grow")
+	searchCmd.Flags().Int64P("growthlt", "l", 0, "Search for crops that take the specified time or less to grow")
+	searchCmd.Flags().StringP("season", "s", "", "Search by a season. Options: Spring, Summer, Fall")
+	searchCmd.Flags().BoolP("trellis", "t", false, "Search for crops. Options: Spring, Summer, Fall")
 
 	stardewCrops.AddCommand(searchCmd)
 }
