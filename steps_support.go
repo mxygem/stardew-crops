cad863e2271f98743002e08b3a8cb21fec1d96be
Jayson Smith <github@nes.33mail.com>
2018-12-17 15:44:58 -0700

    Switch to exec.Command

--- steps_support.go
+++ steps_support.go
@@ -1,18 +1,26 @@
 package main
 
 import (
-	"github.com/jaysonesmith/stardew-crops/commands"
-	"github.com/jaysonesmith/stardew-crops/utils"
+	"log"
+	"os/exec"
 )
 
 // StardewCropsCommand wraps the call to the root
 // command and returns the captured output
 func StardewCropsCommand(args []string) string {
-	so, r, w := utils.STDOutUp()
+	// so, r, w := utils.STDOutUp()
 
-	cmd := commands.StardewCrops
-	cmd.SetArgs(args)
-	cmd.Execute()
+	// cmd := commands.StardewCrops
+	// cmd.SetArgs(args)
+	// cmd.Execute()
 
-	return string(utils.STDOutDown(so, r, w))
+	// return string(utils.STDOutDown(so, r, w))
+
+	cmd := exec.Command("stardew-crops", args[0], args[1])
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatalf("cmd.Run() failed with %s\n", err)
+	}
+
+	return string(out)
 }
