9b345c0fd9c7b8d4d8639ca32d6937b18afa2085
jaysonesmith <github@nes.33mail.com>
2018-12-24 12:07:43 -0700

    Refactor info to output cropdata for consitency/ease of pretty format

--- processors/info.go
+++ processors/info.go
@@ -7,18 +7,18 @@ import (
 )
 
 // Info returns the data around the specified crop
-func Info(args ...string) (data.Crop, error) {
+func Info(args ...string) (data.CropData, error) {
 	if len(args) == 0 || args[0] == "" {
-		return data.Crop{}, fmt.Errorf("No crop name specified, please try again")
+		return data.CropData{}, fmt.Errorf("No crop name specified, please try again")
 	}
 
 	for _, crop := range cropData.Crops {
 		if args[0] == crop.Name {
-			return crop, nil
+			return data.CropData{Crops: []data.Crop{crop}}, nil
 		}
 	}
 
-	return data.Crop{}, fmt.Errorf("Unable to find matching crop for %s", args[0])
+	return data.CropData{}, fmt.Errorf("Unable to find matching crop for %s", args[0])
 }
 
 func init() {
--- processors/info_test.go
+++ processors/info_test.go
@@ -13,37 +13,39 @@ func TestInfo(t *testing.T) {
 	testCases := []struct {
 		name     string
 		input    string
-		expected data.Crop
+		expected data.CropData
 		err      error
 	}{
 		{
 			name:     "Empty args gets an error",
 			input:    "",
-			expected: data.Crop{},
+			expected: data.CropData{},
 			err:      fmt.Errorf("No crop name specified, please try again"),
 		},
 		{
 			name:     "Unmatched crop returns an error",
 			input:    "banana",
-			expected: data.Crop{},
+			expected: data.CropData{},
 			err:      fmt.Errorf("Unable to find matching crop for banana"),
 		},
 		{
 			name:  "Matched crop gets its data returned",
 			input: "garlic",
-			expected: data.Crop{
-				Name: "garlic",
-				Info: data.Info{
-					Description: "Adds a wonderful zestiness to dishes. High quality garlic can be pretty spicy.",
-					Seed:        "garlic seeds",
-					GrowthTime:  4,
-					Season:      []string{"spring"},
-					Continual:   false,
-				},
-				SeedPrices: data.Prices{GeneralStore: 40},
-				Recipes:    []string{"Escargot", "Fiddlehead Risotto", "Oil of Garlic"},
-				Notes:      []string{"Only available starting in year 2"},
-			},
+			expected: data.CropData{
+				Crops: []data.Crop{
+					data.Crop{
+						Name: "garlic",
+						Info: data.Info{
+							Description: "Adds a wonderful zestiness to dishes. High quality garlic can be pretty spicy.",
+							Seed:        "garlic seeds",
+							GrowthTime:  4,
+							Season:      []string{"spring"},
+							Continual:   false,
+						},
+						SeedPrices: data.Prices{GeneralStore: 40},
+						Recipes:    []string{"Escargot", "Fiddlehead Risotto", "Oil of Garlic"},
+						Notes:      []string{"Only available starting in year 2"},
+					}}},
 			err: nil,
 		},
 	}
